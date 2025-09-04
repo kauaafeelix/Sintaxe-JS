2b02f91d382c962673117b67d5d420dae1a07ba5
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-04 14:18:06 -0300

    refactor

--- q3.js
+++ q3.js
@@ -1,7 +1,5 @@
 //3 - Mostrar os múltiplos de 3 entre 1 e 30.
 
-const prompt = require('prompt-sync')();
-
 for (i = 1; i<= 30;i++ ){
     if(i %3 == 0){
         console.log (i+" é multiplo de 3");
