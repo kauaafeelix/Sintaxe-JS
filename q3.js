05f59aab55d6f0d48ab7a670c2d97653cda8ca13
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-04 14:04:20 -0300

    feat: finzaliza exercicio 3 da lista

--- q3.js
+++ q3.js
@@ -1,1 +1,12 @@
 //3 - Mostrar os múltiplos de 3 entre 1 e 30.
+
+const prompt = require('prompt-sync')();
+
+for (i = 1; i<= 30;i++ ){
+    if(i %3 == 0){
+        console.log (i+" é multiplo de 3");
+    }
+    else{
+        console.log(i);
+    }
+}
