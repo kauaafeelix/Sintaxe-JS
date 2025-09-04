0d1b3810c0b13b2bf53841f19572c07b219a467f
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-04 18:12:09 -0300

    feat: finaliza exercicio 4 da lista

--- q4.js
+++ q4.js
@@ -1,1 +1,15 @@
 //4 - Calcular o fatorial de um número.
+
+const prompt = require('prompt-sync')();
+
+const numero = Number (prompt("Digite um número: "));
+
+if (numero > 0) {
+    let resultado = 1;
+    for (let i = numero; i > 0; i--) {
+        resultado *= i;
+    }
+    console.log("O fatorial de "+numero+ " é: "+resultado);
+} else {
+    console.log("Digite um número maior que 0.");
+}
