fb2737721c026312a1745ba1a7bc7c8fd9da01e2
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-05 18:32:37 -0300

    feat: finaliza exercicio 7 dsa lista

--- q7.js
+++ q7.js
@@ -1,1 +1,13 @@
 //7 - Verificar se uma palavra é um palíndromo.
+
+const prompt = require('prompt-sync')();
+
+let palavra = prompt("Digite uma palavra: ");
+
+for(let i = 0, j = palavra.length - 1; i < j; i++, j--){
+    if(palavra[i]!== palavra[j]){
+        console.log(palavra+" não é um palíndromo.");
+    }else{
+        console.log(palavra+" é um palíndromo.");
+    }
+}
