c7f904393a9f270b9ee8cccf05dc8a984e4023a2
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-05 02:26:45 -0300

    feat: finaliza exercicio 6 da lista

--- q6.js
+++ q6.js
@@ -1,1 +1,8 @@
 //6 - Converter uma temperatura em Celsius para Fahrenheit.
+const prompt = require('prompt-sync')();
+
+let temperatura = prompt("Digite a Temperatura em Celsius: ");
+
+let conversao = temperatura *1.8 + 32;
+
+console.log(temperatura+" graus Celsius em Fahrenheit é: "+conversao+" F");
