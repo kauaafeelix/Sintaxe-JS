4a329fe849a64a037394d083cc0b34cb7499e4af
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-05 19:45:28 -0300

    feat: finaliza exercicio 11

--- q11.js
+++ q11.js
@@ -3,3 +3,19 @@
 // Se divisível por 5, exiba "Buzz".
 // Se divisível por 3 e 5, exiba "FizzBuzz".
 // Caso contrário, exiba o número.
+
+for (let i= 1; i <=30; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log(i+": FizzBuzz");
+    }
+    else if(i % 3 == 0){
+        console.log (i+": Fizz");
+    }
+    else if(i % 5 == 0){
+        console.log(i+": Buzz")
+    }
+    else{
+        console.log(i+": nem Fizz, nem Buzz ")
+    }
+
+}
