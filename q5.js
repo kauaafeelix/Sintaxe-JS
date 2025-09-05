333edf1e17e299baea230ecafb12ba19cafbb3b9
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-05 02:12:54 -0300

    feat: finaliza exercicio 5 da lista

--- q5.js
+++ q5.js
@@ -1,1 +1,11 @@
 //5 - Verificar se um ano é bissexto.
+
+let ano = parseInt(prompt("Digite um ano: "));
+
+let bissexto = (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0);
+
+if (bissexto) {
+    console.log( ano+"é um ano bissexto!");
+} else {
+    console.log(ano+ "não é um ano bissexto!");
+}
