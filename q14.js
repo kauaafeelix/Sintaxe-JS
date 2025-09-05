358767d8654a3b92f7c698a550bcfb41184f30f1
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-05 19:46:15 -0300

    feat: finaliza exercicio 14

--- q14.js
+++ q14.js
@@ -1,1 +1,5 @@
 // 14 - Declare uma variável com um texto, por exemplo, "JavaScript". Exiba no console o texto invertido ("tpircSavaJ").
+
+const mensagem = "JavaScript";
+const mensagemInvertida = mensagem.split("").reverse().join("");
+console.log(mensagemInvertida);
