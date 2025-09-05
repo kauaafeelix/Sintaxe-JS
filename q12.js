8a8ed9aae3ff2bf11a7f7bca238416bad058ce4c
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-05 19:45:43 -0300

    feat: finaliza exercicio 12

--- q12.js
+++ q12.js
@@ -1,1 +1,12 @@
 // 12 - Exiba no console a sequência de números de 1 a 5 em uma única linha no formato "1-2-3-4-5".
+
+let resultado = "";
+
+for(let i = 1; i<=5; i++){
+    resultado += i;
+
+    if(i < 5){
+        resultado += "-";
+    }
+}
+console.log (resultado);
