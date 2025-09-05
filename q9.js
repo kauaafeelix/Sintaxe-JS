69fb4ebcd481d576801eeb7079f2fea4a9cafd90
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-05 19:04:28 -0300

    feat: finaliza exercicio 9

--- q9.js
+++ q9.js
@@ -1,1 +1,9 @@
 //9 - Exiba no console a tabuada do número 5, do 1 ao 10.
+
+let numero = 5;
+
+for (let i = 1; i<=10; i++){
+    let tabuada = numero*i;
+
+    console.log(numero+" x "+i+ " = "+tabuada)
+}
