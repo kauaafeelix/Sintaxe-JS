89783a2dcda1d22cef2b1ad5b679890521efd253
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:47:01 -0300

    finalzia exercicio 15 da lista 3

--- lista3/q15.js
+++ lista3/q15.js
@@ -1,2 +1,9 @@
 // 15 - Crie um objeto chamado "pessoa" que tenha as propriedades "nome", "idade" e "profissão".
 // Atribua valores a essas propriedades e imprima o objeto no console.
+
+const pessoa = {
+    nome: "Kauã",
+    idade: 18,
+    profissao: "Estudante"
+};
+console.log(pessoa);
