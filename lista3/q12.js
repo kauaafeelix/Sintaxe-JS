d95db9261dc5cba538fbe36d78f80cacb805fb8f
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:46:42 -0300

    finalzia exercicio 12 da lista 3

--- lista3/q12.js
+++ lista3/q12.js
@@ -1,1 +1,10 @@
 // 12 - Criar uma função que junte dois arrays e retorno o resultado como um novo array.
+
+function juntarArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const resultado = juntarArrays(array1, array2);
+console.log(resultado);
