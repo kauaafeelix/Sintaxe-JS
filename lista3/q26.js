7cecad335bbf60b3182b69a7dde962730afa7a3d
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:48:15 -0300

    finalzia exercicio 26 da lista 3

--- lista3/q26.js
+++ lista3/q26.js
@@ -2,3 +2,19 @@
 // parâmetro e retorna a mesma string em letras.
 // console.log(converterMorse(".- -... -.-.")); // Exemplo de saída: "abc"
 // console.log(converterMorse("--. --- --- -.. -.. --- --. .-.. ..- --. .... --- --.")); // Exemplo de saída: "good morning"
+
+function converterMorse(morse) {
+    const codigoMorse = {
+        ".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e",
+        "..-.": "f", "--.": "g", "....": "h", "..": "i", ".---": "j",
+        "-.-": "k", ".-..": "l", "--": "m", "-.": "n", "---": "o",
+        ".--.": "p", "--.-": "q", ".-.": "r", "...": "s", "-": "t",
+        "..-": "u", "...-": "v", ".--": "w", "-..-": "x", "-.--": "y",
+        "--..": "z", "-----": "0", ".----": "1", "..---": "2", "...--": "3",
+        "....-": "4", ".....": "5", "-....": "6", "--...": "7", "---..": "8",
+        "----.": "9", "/": " "
+    };
+    return morse.split(' ').map(code => codigoMorse[code] || '').join('');
+}
+console.log(converterMorse(".- -... -.-.")); 
+console.log(converterMorse("--. --- --- -.. -.. --- --. .-.. ..- --. .... --- --."));
