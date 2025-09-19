12f0aa37eb374858617f15f98acab45b06bf55e6
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:47:28 -0300

    finalzia exercicio 19 da lista 3

--- lista3/q19.js
+++ lista3/q19.js
@@ -1,3 +1,7 @@
 // 19 - Crie um novo array chamado "idades" com algumas idades. Use o método every() para verificar se
 // todos os elementos do array são maiores que 18. Imprima o resultado no console.
 
+
+const idades = [20, 25, 30, 17, 22];
+const todasMaioresQue18 = idades.every(idade => idade > 18);
+console.log(todasMaioresQue18);
