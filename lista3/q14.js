34d2de37e5e96d18aaa4c205baac5611fa72d380
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:46:56 -0300

    finalzia exercicio 14 da lista 3

--- lista3/q14.js
+++ lista3/q14.js
@@ -1,2 +1,14 @@
 // 14 - Escreva uma função em JavaScript que conte quantas vezes um caractere aparece em uma string.
 // Tanto o caractere quanto a string devem ser informadas.
+
+function contarCaracteres(str, char) {
+    let contador = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            contador++;
+        }   
+    }
+    return contador;
+}
+
+console.log(contarCaracteres("javascript", "a"));
