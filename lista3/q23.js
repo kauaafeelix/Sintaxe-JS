d62de63555b6d788bdf2ace4715fcfe148792232
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:47:56 -0300

    finalzia exercicio 23 da lista 3

--- lista3/q23.js
+++ lista3/q23.js
@@ -1,2 +1,13 @@
 // 23 - Crie uma função chamada "gerarSenha" que recebe um número como parâmetro e retorna uma
 // senha aleatória com esse número de caracteres.
+
+function gerarSenha(tamanho) {
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let senha = '';
+    for (let i = 0; i < tamanho; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        senha += caracteres.charAt(indiceAleatorio);
+    }
+    return senha;
+}
+console.log(gerarSenha(10));
