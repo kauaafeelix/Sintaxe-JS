80ea8e3f660c30e4b9c55c311130292d64adbfef
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:47:08 -0300

    finalzia exercicio 16 da lista 3

--- lista3/q16.js
+++ lista3/q16.js
@@ -1,2 +1,10 @@
 // 16 - Adicione uma nova propriedade chamada "hobbies" ao objeto "pessoa" criado anteriormente.
 // Atribua a essa propriedade um array de hobbies que você gosta. Imprima o objeto no console.
+
+const pessoa = {
+    nome: "Kauã",
+    idade: 18,
+    profissao: "Estudante"
+};
+pessoa.hobbies = ["Jogar Futebol", "Jogar Video Game", "Programar"];
+console.log(pessoa);
