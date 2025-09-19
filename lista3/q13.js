11b280b5d9270dedc55d48bb135f63eb1fb595b9
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:46:49 -0300

    finalzia exercicio 13 da lista 3

--- lista3/q13.js
+++ lista3/q13.js
@@ -8,3 +8,14 @@
 // t = Tempo do investimento, em meses
 // Exiba o resultado com duas casas decimais.
 
+
+function calcularMontante(capital, taxaJuros, tempoMeses) {
+    let montante = capital * Math.pow((1 + taxaJuros / 100), tempoMeses);
+    return montante.toFixed(2);
+}
+
+const capitalInicial = 1000;
+const taxaDeJuros = 5;
+const tempoDeInvestimento = 12;
+const montanteFinal = calcularMontante(capitalInicial, taxaDeJuros, tempoDeInvestimento);
+console.log("O montante final do investimento é: R$ " + montanteFinal);
