96bf483ce49ac38514051006f29d0738841de247
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:47:43 -0300

    finalzia exercicio 21 da lista 3

--- lista3/q21.js
+++ lista3/q21.js
@@ -1,2 +1,5 @@
 // 21 - Crie uma variável chamada "raizQuadrada" que armazene a raiz quadrada de 64 usando o método
 // Math.sqrt(). Imprima o valor no console.
+
+const raizQuadrada = Math.sqrt(64);
+console.log(raizQuadrada);
