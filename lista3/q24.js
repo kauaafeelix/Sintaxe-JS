73925d87eef2bf84dc3798171ce383dfb94cea30
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:48:03 -0300

    finalzia exercicio 24 da lista 3

--- lista3/q24.js
+++ lista3/q24.js
@@ -1,2 +1,8 @@
 // 24 - Crie uma função chamada "gerarNumeroAleatorio" que recebe um número mínimo e um número
 // máximo como parâmetros e retorna um número aleatório entre esses dois números.
+
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(gerarNumeroAleatorio(1, 10));
