1061df5b2c2cb5afa481d7ffa154b176246733d8
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:48:23 -0300

    finalzia exercicio 27 da lista 3

--- lista3/q27.js
+++ lista3/q27.js
@@ -1,2 +1,20 @@
 // 27 - Crie uma função chamada "criarMatriz" que recebe o número de linhas e o número de colunas
 // como parâmetros e retorna uma matriz com essas dimensões preenchida com números aleatórios.
+
+
+function criarMatriz(linhas, colunas) {
+    const matriz = [];
+    for (let i = 0; i < linhas; i++) {
+        const linha = [];
+        for (let j = 0; j < colunas; j++) {
+            linha.push(Math.floor(Math.random() * 100));
+        }
+        matriz.push(linha);
+    }
+    return matriz;
+}
+
+const linhas = 3;
+const colunas = 4;
+const matrizAleatoria = criarMatriz(linhas, colunas);
+console.log(matrizAleatoria);
