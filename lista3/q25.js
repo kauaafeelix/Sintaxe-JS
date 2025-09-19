e650064097eee3c1aa0700bf187cc660c40e9bc4
Kaua Felix <kfelixcosta11@gmail.com>
2025-09-19 19:48:10 -0300

    finalzia exercicio 25 da lista 3

--- lista3/q25.js
+++ lista3/q25.js
@@ -1,2 +1,13 @@
 // 25 - Crie uma função chamada "calculadora" que recebe uma string como parâmetro contendo uma
 // expressão matemática (ex: "2+3*4") e retorna o resultado dessa expressão.
+
+function calculadora(expressao) {
+    try {
+        const resultado = eval(expressao);
+        return resultado;
+    } catch (error) {
+        return "Expressão inválida";
+    }
+}
+
+console.log(calculadora("2+3*4"));
