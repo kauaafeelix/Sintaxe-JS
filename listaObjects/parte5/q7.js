492b543bea9b034cdcd3da0d34fb6dac30cb71fc
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-26 20:49:44 -0300

    Parte 5: Objetos Aninhados (Objetos dentro de Objetos) (Detalhes do Usuário)

---
+++ listaObjects/parte5/q7.js
@@ -0,0 +1,11 @@
+let usuario ={
+    nome: "Kauã Felix",
+    endereco : {
+        rua: "Rua das Flores",
+        numero: 123,
+        cidade: "Jaraguá do Sul"
+    }
+}
+
+console.log("O usuário " + usuario.nome + " mora na cidade de " + usuario.endereco.cidade + ", na rua " + usuario.endereco.rua +"."); 
+
