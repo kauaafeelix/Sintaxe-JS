b3a6bffea880db245dfdf4e741e58589d328167e
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-26 20:50:13 -0300

    Parte 5: Objetos Aninhados (Objetos dentro de Objetos) (Informações da Matrícula)

---
+++ listaObjects/parte5/q8.js
@@ -0,0 +1,11 @@
+let aluno ={
+    nome : 'Kauã Felix',
+    matricula:{
+        numero: 123456,
+        curso: 'Desenvolvimento de Sistemas'
+    }
+};
+
+aluno.matricula.numero = 234567;
+
+console.log(aluno);
