aa6e23f965aaaea3b986e1e5cd7d4bb3d4f1a930
Kauã Felix <kfelixcosta11@gmail.com>
2025-09-26 20:48:02 -0300

    Parte 2: Acesso a Propriedades (Detalhes de um Filme (Notação de Ponto))

---
+++ listaObjects/parte2/q3.js
@@ -0,0 +1,10 @@
+let filme ={
+    titulo : 'Inception',
+    ano : 2010,
+    diretor : 'Christopher Nolan',
+    genero: 'Ficção Cientifica'
+};
+
+console.log(filme.titulo);
+console.log(filme.diretor);
+
